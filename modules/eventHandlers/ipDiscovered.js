4c7bd6d09491b174bbec2c290b5337e6abb3cbda
Krasimir Kostadinov <krazymir@gmail.com>
2017-12-23 23:23:56 +0200

    Added IRC nick encoding of ip/port Added mocha tests

--- modules/eventHandlers/ipDiscovered.js
+++ modules/eventHandlers/ipDiscovered.js
@@ -4,15 +4,14 @@ const common = require('../common')
 const settings = common.settings
 const ircClient = require('../ircClient')
 
-
 function getIrcNodes(serverAddress, port, nick, realName, channel, say) {
     ircClient.talk(serverAddress, nick, realName, channel, say)
 }
 
 common.emitter.on('ipDiscovered', () => {
-    let say = `${common.config.externalIP.replace(/[.]/g, '_')}&${settings.api.port}`
-    let nick = 'guest' + Math.floor(Math.random()*999999)
+    let nick = ircClient.ipPortToNick(common.config.externalIP, settings.api.port)
+    
     settings.irc.servers.forEach(server => {
-        getIrcNodes(server.address, server.port, nick, 'LanecDB Node', `#LanecDB@${settings.nodes.clusterName}`, say)
+        ircClient.listen(server.address, nick, 'LanecDB Node', `#LanecDB@${settings.nodes.clusterName}`, 'nodeDiscovered')
     });
 })
--- modules/ircClient/index.js
+++ modules/ircClient/index.js
@@ -4,11 +4,19 @@ const common = require('../common')
 const channels = require('irc-channels')
 const irc = require("irc-connect")
 
+function ipPortToNick(ip, port) {
+    // Some sanity checks - we want valid IPs and numeric ports bigger than 999
+    if(!ip || !port || !/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/.test(ip) || (isNaN(parseInt(port, 10)) || (port < 1000))) {
+        return null
+    }
+    let tetrad = ip.split('.')
+    return `_${tetrad[0].toString(16).padStart(2, '0')}${tetrad[1].toString(16).padStart(2, '0')}${tetrad[2].toString(16).padStart(2, '0')}${tetrad[3].toString(16).padStart(2, '0')}${port.toString(16)}`
+}
+
 function talk(address, nick, realName, channel, say) {
     let client = irc.connect(address, { nick: nick, realname: realName }).use(irc.pong, channels).on('welcome', function (msg) {
         this.join(channel, function (chn) {
             chn.msg(say)
-            chn.part()
         })
     })
 }
@@ -25,5 +33,6 @@ function listen(address, nick, realName, channel, eventName) {
 
 module.exports = {
     talk: talk,
-    listen: listen
+    listen: listen,
+    ipPortToNick: ipPortToNick
 }
