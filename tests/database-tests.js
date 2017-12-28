ec5669bc3cba71e4b50a14ab831fa6373a85564f
Krasimir Kostadinov <krazymir@gmail.com>
2017-12-28 03:29:23 +0200

    Adding tests for the async methods

--- tests/database-tests.js
+++ tests/database-tests.js
@@ -12,7 +12,7 @@ let updatedValue = {
     prop: 7,
     propString: 'New string'
 }
-describe('database', function () {
+describe('database - sync', function () {
     it('deleteSync(key) should work', function () {
         db.deleteSync(expectedKey)
     })
@@ -34,5 +34,33 @@ describe('database', function () {
     })
     it('deleteSync(key) should delete the created values', function () {
         assert.equal(db.deleteSync(expectedKey), true)
+    })
+})
+describe('database - async', function () {
+    it('delete(key, cb) should work', function () {
+        db.delete(expectedKey, () => { })
+    })
+    it('set(key, value, cb) should save values', function () {
+        db.set(expectedKey, expectedValue, (data) => { })
+    })
+    it('get(key, cb) should be able to get the values we set', function () {
+        db.get(expectedKey, (err, data) => {
+            assert.equal(expectedValue.prop, data.prop)
+            assert.equal(expectedValue.propString, data.propString)
+        })
+    })
+    it('set(key, value, cb) should be able to update the value', function () {
+        db.set(expectedKey, updatedValue, () => { })
+    })
+    it('get(key, cb) should be able to get the value we set', function () {
+        db.getSync(expectedKey, (err, data) => {
+            assert.equal(updatedValue.prop, data.prop)
+            assert.equal(updatedValue.propString, data.propString)
+        })
+    })
+    it('delete(key, cb) should delete the created values', function () {
+        db.delete(expectedKey, () => {
+            assert.equal(db.getSync(expectedKey), null)
+        })
     })
 })
