9931fb4b6395ff79aa3f89c73d12b5d738a75cf1
Krasimir Kostadinov <krazymir@gmail.com>
2018-01-08 21:35:53 +0200

    Temporarily disabling failing tests

--- tests/security-tests.js
+++ tests/security-tests.js
@@ -1,17 +1,24 @@
 'use strict';
 
 const assert = require('assert')
-const utils = require('../modules/security')
+const utils = require('../modules/utils')
 
 const data = 'Some string to be encrypted'
 let encryptedData
+let signature
 
 describe('Initialization - encrypt/descrypt', function () {
     it('encrypt(data) should encrypt the data', function () {
-        encryptedData = utils.encrypt(data)
+        encryptedData = utils.security.encrypt(data)
     })
     it('decrypt(data, privateKey) should decrypt the encrypted data', function () {
-        let decryptedData = utils.decrypt(encryptedData)
+        let decryptedData = utils.security.decrypt(encryptedData)
         assert.equal(decryptedData, data, 'The decripted string must mach the string before encryption')
+    })
+    it('sign(data, key) should sign the data and return a signature', function () {
+        signature = utils.security.sign(data)
+    })
+    it('verifySignature(data, signature, key) should verify the data signature', function () {
+        assert.equal(utils.security.verifySignature(data, signature, utils.security.getPublicKey()), true, 'The signature should be ok')
     })
 })
